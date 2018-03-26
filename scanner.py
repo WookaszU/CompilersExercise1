db5c57d7fb2ff8451140b4020741442840aea9af
Lukasz <l.uchman@interia.pl>
2018-03-26 22:57:59 +0200

    v0.4 error message line number added

--- scanner.py
+++ scanner.py
@@ -1,4 +1,3 @@
-
 import ply.lex as lex
 
 
@@ -53,12 +52,6 @@ reserved = {
     'print' : 'PRINT'
 }
 
-#When building the master regular expression, rules are added in the following order:
-
-#1 All tokens defined by functions are added in the same order as they appear in the lexer file.
-#2 Tokens defined by strings are added next by sorting them in order of decreasing regular expression length
-#  (longer expressions are added first).
-
 
 t_DOTADD    = r'\.\+'
 t_DOTSUB   = r'\.-'
@@ -76,23 +69,10 @@ t_NE = r'!='
 t_E = r'=='
 
 
-# t_IF = r'if'
-# t_ELSE = r'else'
-# t_FOR = r'for'
-# t_WHILE = r'while'
-#
-# t_BREAK = r'break'
-# t_CONTINUE = r'continue'
-# t_RETURN = r'return'
-#
-# t_EYE = r'eye'
-# t_ZEROS = r'zeros'
-# t_ONES = r'ones'
-# t_PRINT = r'print'
-
-#ignorowaneSymbole
+#ignorowane znaki
 t_ignore = '  \t'
 
+#ignorowanie komentarzy
 t_ignore_COMMENT = r'\#.*'
 
 
@@ -101,18 +81,17 @@ def t_FLOATNUM(t):
     t.value = float(t.value)
     return t
 
+
 def t_INTNUM(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
+
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
-
-
-
 
 
 def t_newline(t):
@@ -121,13 +100,13 @@ def t_newline(t):
 
 
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print("Illegal character {first} at line {second}".format(first=t.value[0], second=t.lexer.lineno))
     t.lexer.skip(1)
 
 
 # Compute column.
-#     input is the input text string
-#     token is a token instance
+# input is the input text string
+# token is a token instance
 def find_column(input, token):
     line_start = input.rfind('\n', 0, token.lexpos) + 1
     return (token.lexpos - line_start) + 1
